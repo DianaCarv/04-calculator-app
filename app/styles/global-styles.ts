418f243feb0c5946f91d5b5d45d886d5f6c31519
Diana Carvajal <dianalorenacarvajal2@gmail.com>
2025-02-27 11:04:47 -0500

    add components and configuration

---
+++ app/styles/global-styles.ts
@@ -0,0 +1,13 @@
+import { Colors } from "@/constants/Colors";
+import { ImageBackground, StyleSheet } from "react-native";
+
+
+
+export const globalStyles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: Colors.background, 
+
+     },
+
+});
